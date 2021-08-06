6b48699fded7911da21f21d5646aedd866e1e97b
Charlie <charlieyeh1114@gmail.com>
2021-08-07 02:45:45 +0800

    confirm monthpicker

--- RegisterAndLogin_Consumer/vendors/js/jquery.mtz.monthpicker.js
+++ RegisterAndLogin_Consumer/vendors/js/jquery.mtz.monthpicker.js
@@ -66,7 +66,7 @@
                         selectedMonthName: '',
                         selectedYear: year,
                         startYear: year - 10,
-                        finalYear: year + 50,
+                        finalYear: year + 20,
                         monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
                         id: "monthpicker_" + (Math.random() * Math.random()).toString().replace('.', ''),
                         openOnFocus: true,
